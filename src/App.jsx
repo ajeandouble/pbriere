72716e87052a0fc52359aab2767154271066fc70
ajean <jean.adrien.mtpl@gmail.com>
2020-04-13 21:58:06 +0200

    Beta

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ import Vids from './components/Vids'
 import Info from './components/Info'
 
 const mainLinks = [['img/', '#'], ['vids/', '/vids'], ['url+', '#'], ['info/', '/info']];
-const imgLinks =  [['papier thermique', '#'], ['random/', '#']]; 
+const imgLinks =  [['papier thermique', '#'], ['random/', '#']];
 const papierLinks = [['pa. th. 0/', '/img/0'], ['pa. th. 1/', '/img/1'], ['pa. th. 2/', '/img/2']];
 const randomLinks = [['photos/', '/img/3'], ['crbn', '/img/4']];
-const urlLinks = [['tumblr/', 'https://aiglemangeurdesinges.tumblr.com/'], ['tumblr2/', 'https://friction-images.tumblr.com/'], ['flickr/', 'https://www.flickr.com/photos/tofusoyo/'], ['ig/', 'https://www.instagram.com/hermetikon/'], ['ig2/', 'https://www.instagram.com/visual_predation/'], ['snd/', 'https://soundcloud.com/prothese']];
+const urlLinks = [['tumblr/', 'https://aiglemangeurdesinges.tumblr.com/'], ['tumblr2/', 'https://friction-images.tumblr.com/'], ['flickr/', 'https://www.flickr.com/photos/tofusoyo/'], ['ig/', 'https://www.instagram.com/hermetikon/'], ['ig2/', 'https://www.instagram.com/visual_predation/'], ['soundcloud/', 'https://soundcloud.com/prothese']];
 
 const Links = () => {
   const [activeLinks, setActiveLinks] = useState({ img: false, papier: false, random: false, url: false });
@@ -83,7 +83,7 @@ const Links = () => {
           <ul className="links--url">
             {/* padding */}
             {urlLinks.map((element, i) =>  <div><span className="links--url--padding" style={{ visibility: 'hidden' }}>{ window.innerWidth < 640 ? "PaddingPaddin" : "PaddingPaddingPaddingPaddin" }</span>
-<li className="links--img--li" key={element[1]}><Link to={`/url/${i}`} target="_blank">{element[0]}</Link><br /></li></div>)}
+            <li className="links--img--li" key={element[1]}><Link to={`/url/${i}`} target="_blank">{element[0]}</Link><br /></li></div>)}
           </ul>
         </nav>
         : false }
@@ -111,6 +111,7 @@ function App() {
           <Route path="/img/:id" render={(props) => <Img {...props} isAuthed={true} />} />
           <Route path="/info"><Info /></Route>
           <Route path="/vids"><Vids /></Route>
+          { urlLinks.map((url, i) => <Route path={`/url/${i}`} component={ () => { window.location.href = urlLinks[i][1]; return null; } } />) }
         </Switch>
       </div>
     </Router>
